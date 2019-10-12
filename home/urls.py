4464343f6da1c95b89335a7a43ad2c7727fe25e2
idelfrides <idelfridesjorgepapai@gmail.com>
2019-10-12 12:27:37 -0300

    First commit on submodule gestao_clientes

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import home, my_logout
+from .views import home, my_logout, HomePageView, Myview
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('logout/', my_logout, name="logout"),
+    path('home2/', TemplateView.as_view(template_name='home2.html')),
+    path('home3/', HomePageView.as_view(template_name='home3.html')),
+
+    path('view/', Myview.as_view()),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+from django.views import View
 
+
+# TODO: test to do here
 def home(request):
+    # import pdb
+    # pdb.set_trace()    # pára a execução nesta linha,
+                       # permitindo vc debugar o script 
     return render(request, 'home.html')
 
 
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+
+class HomePageView(TemplateView):
+    """ template views """
+    template_name='home3.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_test'] = 'This is a test of get_context_data'
+        return context
+
+
+class Myview(View):
+    
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello World')
+
+
+
+# FBV - Function-based views
+# CBV - Class-based views
